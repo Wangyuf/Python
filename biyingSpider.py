791c84af9f24b550f1be3fab0cf3f65e2f0c854b
2587357943@qq.com <2587357943@qq.com>
2017-07-12 23:59:52 +0800

    update code

--- biyingSpider.py
+++ biyingSpider.py
@@ -1,14 +1,31 @@
 import requests
 import re
+import os
 import time
-local = time.strftime("%Y.%m.%d")
-url = 'http://cn.bing.com/'
-con = requests.get(url)
-content = con.text
-reg = r"(http://s.cn.bing.net/az/hprichbg/rb/.*?.jpg)"
-a = re.findall(reg, content, re.S)[0]
-print(a)
-read = requests.get(a)
-f = open('%s.jpg' % local, 'wb')
-f.write(read.content)
-f.close()
+local = time.strftime("%Y.%m.%d %H:%M")
+
+picPathDir = 'bingPic'
+if os.path.isdir(picPathDir) == False:
+    os.makedirs(picPathDir)
+monthPath = time.strftime("%Y.%m")
+if os.path.isdir(picPathDir+'/'+monthPath) == False:
+    os.makedirs(picPathDir+'/'+monthPath)
+
+localDate = time.strftime("%Y.%m.%d")
+datePicPath = picPathDir+'/'+monthPath+'/'+localDate + '.jpg';
+if os.path.isfile(datePicPath) :
+        print(localDate+': '+localDate+'.jpg'+' pic exists')
+else:
+    url = 'http://cn.bing.com/'
+    con = requests.get(url)
+    content = con.text
+    p = re.compile(r"(/az/hprichbg/rb/.*?.jpg)")
+    a = p.findall(content)
+    if len(a) >0:
+        read = requests.get(url+a[0])
+        f = open(datePicPath, 'wb')
+        f.write(read.content)
+        f.close()
+        print(local+': '+localDate+'.jpg'+' down pic success')
+    else:
+        print(local+': bing pic not find')
--- douban_book.py
+++ douban_book.py
@@ -19,34 +19,48 @@ def get_con(html):
     book_list = soup.find('div', attrs={'class': 'article'})
     page = soup.find('div', attrs={'class': 'paginator'})
     next_page = page.find('span', attrs={'class': 'next'}).find('a')
-    name = []
+    name_list = []
     for i in book_list.find_all('table'):
         book_name = i.find('div', attrs={'class': 'pl2'})
+        intro_name = i.find('p', attrs={'class': 'quote'})
         m = list(book_name.find('a').stripped_strings)
+        if (intro_name):
+            n = list(intro_name.find('span').stripped_strings)
+        else:
+            n = ['no intro']
         if len(m)>1:
             x = m[0]+m[1]
         else:
             x = m[0]
+
+        if len(n)>1:
+            y = n[0]+n[1]
+        else:
+            y = n[0]
         #print(x)
+        name = []
         name.append(x)
+        name.append(y)
+        name_list.append(name);
     if next_page:
-        return name, next_page.get('href')
+        return name_list, next_page.get('href')
     else:
-        return name, None
+        return name_list, None
 
 
 def main():
     url = 'https://book.douban.com/top250'
     name_list=[]
     while url:
+        print("visited:"+url)
         html = get_html(url)
         name, url = get_con(html)
-        name_list = name_list + name
+        if url != None:
+            print("return url:"+url)
+        print(name)
+        name_list += name;
     for i in name_list:
-        location = 'A%s'%(name_list.index(i)+1)
-        print(i)
-        print(location)
-        ws1[location]=i
+        ws1.append(i)
     wb.save(filename=excel_name)
 
 
